a5c7bb11ad6f818de553de530796637b5cb370cf
Steryyy <dev.8b054@simplelogin.co>
2023-06-09 19:56:23 +0200

    Update qwik, update code style add deno.json

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,27 +1,26 @@
 import { component$, Slot } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "~/components/starter/header/header";
-import Footer from "~/components/starter/footer/footer";
-
-import { qwikify$ } from "@builder.io/qwik-react";
-
-import { FaBars } from "react-icons/fa";
-const FaQwik = qwikify$(FaBars);
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+import { BsList } from "@qwikest/icons/bootstrap";
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+	// Control caching for this request for best performance and to reduce hosting costs:
+	// https://qwik.builder.io/docs/caching/
+	cacheControl({
+		// Always serve a cached response by default, up to a week stale
+		staleWhileRevalidate: 60 * 60 * 24 * 7,
+		// Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+		maxAge: 5,
+	});
+};
 
 export default component$(() => {
-  return (
-    <main>
-      <nav class="flex dark:bg-white dark:text-black justify-between z-30 text-white bg-black  p-3  fixed w-full xlg">
-        <span class="hover:font-bold">Meet Me</span>
-        <FaQwik className="hover:rotate-90 transition-transform" />
-      </nav>
-      <Slot />
-    </main>
-  );
+	return (
+		<main>
+			<nav class="flex dark:bg-white dark:text-black justify-between z-30 text-white bg-black  p-3  fixed w-full xlg">
+				<span class="hover:font-bold">Meet Me</span>
+				<BsList class="hover:rotate-90 transition-transform text-xl" />
+			</nav>
+			<Slot />
+		</main>
+	);
 });
